2d9fda8a15486c40891dafbb47adfb4c15650699
Samuel Momoh <samueldan@live.com>
2023-01-11 16:11:17 +0100

    project initialization

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const stripe = require('stripe')('sk_test_51M9RNdBlBeMRnW7CLsA7xYolR754Y3SuigAo3HSD0WCxJls1HpVp6ZtR1BN3mzffHBL8Og7AVLNP6chDEKd7c68g00ZNyKfoGR');
+
+app.get('/', async(req, res) => {
+    try {
+      const paymentMethod = await stripe.paymentMethods.create({
+        type: 'card',
+        card: {
+          number: '4242424242424242',
+          exp_month: 8,
+          exp_year: 2026,
+          cvc: '743',
+        },
+      });
+      res.json(paymentMethod)
+
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
